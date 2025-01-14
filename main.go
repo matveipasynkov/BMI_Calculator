05b5323d8a0951e1f18d050bc879ce5c0ee7aa2e
matveipasynkov <asynkovm05@icloud.com>
2025-01-14 20:19:52 +0300

    functions added + user's input

--- main.go
+++ main.go
@@ -5,9 +5,30 @@ import (
 	"math"
 )
 
+const IMTPower = 2
+
 func main() {
-	const IMTPower = 2
-	userHeight, userKg := 1.8, 100.0
+	fmt.Println("____Калькулятор ИМТ____")
+	userHeight, userKg := getUserInput()
+	IMT := calculateIMT(userKg, userHeight)
+	outputResult(IMT)
+}
+
+func outputResult(IMT float64) {
+	result := fmt.Sprintf("Ваш ИМТ: %.0f\n", IMT)
+	fmt.Print(result)
+}
+
+func calculateIMT(userKg float64, userHeight float64) float64 {
 	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Println(IMT)
+	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight, userKg float64
+	fmt.Print("Введите свой рост (в метрах): ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес (в кг): ")
+	fmt.Scan(&userKg)
+	return userHeight, userKg
 }
