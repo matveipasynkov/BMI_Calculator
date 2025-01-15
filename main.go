91275ad0f2ac2aa977bc1839de9b9dbfd8d88896
matveipasynkov <asynkovm05@icloud.com>
2025-01-15 19:22:09 +0300

    update 2.0
    repeat task + error handler added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -8,20 +9,48 @@ import (
 const IMTPower = 2
 
 func main() {
-	fmt.Println("____Калькулятор ИМТ____")
-	userHeight, userKg := getUserInput()
-	IMT := calculateIMT(userKg, userHeight)
-	outputResult(IMT)
+	for {
+		fmt.Println("____Калькулятор ИМТ____")
+		userHeight, userKg := getUserInput()
+		IMT, err := calculateIMT(userKg, userHeight)
+		if err != nil {
+			fmt.Println("Вес и/или рост некорректны.")
+		} else {
+			outputResult(IMT)
+		}
+		if !checkRepeatCalculation() {
+			break
+		}
+	}
 }
 
 func outputResult(IMT float64) {
 	result := fmt.Sprintf("Ваш ИМТ: %.0f\n", IMT)
 	fmt.Print(result)
+	switch {
+	case IMT < 16:
+		fmt.Println("У вас сильный дефицит массы тела.")
+	case IMT <= 18.5:
+		fmt.Println("У вас дефицит массы тела.")
+	case IMT <= 25:
+		fmt.Println("У вас норма массы тела.")
+	case IMT <= 30:
+		fmt.Println("У вас избыточная масса тела.")
+	case IMT <= 35:
+		fmt.Println("У вас 1-я степень ожирения.")
+	case IMT <= 40:
+		fmt.Println("У вас 2-я степень ожирения.")
+	default:
+		fmt.Println("У вас 3-я степень ожирения.")
+	}
 }
 
-func calculateIMT(userKg float64, userHeight float64) float64 {
+func calculateIMT(userKg float64, userHeight float64) (float64, error) {
+	if userHeight <= 0 || userKg <= 0 {
+		return 0, errors.New("NO_PARAMS_ERROR")
+	}
 	IMT := userKg / math.Pow(userHeight, IMTPower)
-	return IMT
+	return IMT, nil
 }
 
 func getUserInput() (float64, float64) {
@@ -31,4 +60,20 @@ func getUserInput() (float64, float64) {
 	fmt.Print("Введите свой вес (в кг): ")
 	fmt.Scan(&userKg)
 	return userHeight, userKg
+}
+
+func checkRepeatCalculation() bool {
+	var input string
+	for {
+		fmt.Print("Хотите ли вы продолжить работу программы (да / нет)?: ")
+		fmt.Scan(&input)
+		switch input {
+		case "да":
+			return true
+		case "нет":
+			return false
+		default:
+			fmt.Println("Неверный ввод. Попробуйте ещё раз.")
+		}
+	}
 }
